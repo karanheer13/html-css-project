3b9b729cd6199c443a9ac5dd997a347718750368
karanheer13 <73669556+karanheer13@users.noreply.github.com>
2020-11-30 13:55:21 -0800

    updates to nav

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,6 +1,8 @@
 const body = document.body;
 const btn = document.querySelector('.menu-btn');
+const slideoutMenu = document.querySelector('.menu-btn');
 
 btn.addEventListener('click', function(){
     body.classList.toggle('show');
 });
+
